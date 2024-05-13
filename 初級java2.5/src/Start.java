5de3b8f40d8f4ceee7748ba7505d3f669a21e5dc
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-13 11:06:02 +0900

    2章の練習問題です

---
+++ 初級java2.5/src/Start.java
@@ -0,0 +1,24 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		MyFrame frame1 =new RectFrame();
+		frame1.setLocation(350,150);
+		
+		MyFrame frame2 =new RectFrame();
+		frame2.setLocation(200,200);
+		
+		int x,y,ans;
+		x=2;
+		y=3;
+		ans = x * y;
+		
+		System.out.println(ans);
+		
+		
+	}
+
+}
