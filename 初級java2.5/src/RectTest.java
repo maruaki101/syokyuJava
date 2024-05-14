8d56a9cb17a15ffb437129113b0b14d77f66e515
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-14 12:12:06 +0900

    ３－３練習問題を追加

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -3,12 +3,22 @@ public class RectTest extends MyFrame
 {
 	public void run() 
 	{
-		setColor(255,0,0);
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
-		setColor(0,255,0);
+		
+		setColor(200,0,200);
 		fillRect(130,40,50,100);
-		setColor(0,0,255);
+		
+		setColor(0,0255,255);
 		fillRect(30,200,150,10);
+		
+		setColor(0,0,0);
 		fillRect(90,160,30,30);
+		
+		setColor(50,50,50);
+		fillRect(30,300,150,50);
+		
+		setColor(255,0,0);
+		fillRect(80,0,50,350);
 	}
 }
