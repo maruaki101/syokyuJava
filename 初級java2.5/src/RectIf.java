3eedef1839958f37ca37b93da4e1666cac4e4bc6
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-20 12:11:59 +0900

    確認問題４－４追加

--- 初級java2.5/src/RectIf.java
+++ 初級java2.5/src/RectIf.java
@@ -9,18 +9,23 @@ public class RectIf extends MyFrame
 		
 		for(i=0;i<9;i++) 
 		{
+			
+			if(i==0) 
+			{
+				fillRect(x,60,10,100);
+			}
 			/*if(i==4 || i==7) 
 			{
 				fillRect(x,100,10,100);
 			}*/
-			if(i>=4 && i<=7) 
+			/*if(i>=4 && i<=7) 
+			{
+				fillRect(x,100,10,100);
+			}*/
+			else if(i == 8)
 			{
 				fillRect(x,100,10,100);
 			}
-			/*else if(i == 7)
-			{
-				fillRect(x,60,10,100);
-			}*/
 			else
 			{
 				fillRect(x,80,10,100);
