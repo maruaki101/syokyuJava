807220c9b8302f05509a48907e611bbd71bb7e7b
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-21 11:18:05 +0900

    追加課題追加課題追加

--- 初級java2.5/src/Practice4.java
+++ 初級java2.5/src/Practice4.java
@@ -3,15 +3,15 @@ public class Practice4 extends MyFrame
 {
 	public void run() 
 	{
-		int x =30,count =0,y=30, g = 30;
+		int x =30,count =0,y=0;
 		while(true) 
 		{
-			fillRect(y,y,x,x);
+			fillRect(x,80,10,100-y);
+			fillRect(x,200-y,10,100+y);
 			x+=20;
-			y += g;
-			g +=20;
+			y+=10;
 			count++;
-			if(count == 5) 
+			if(count == 10) 
 			{
 				break;
 			}
