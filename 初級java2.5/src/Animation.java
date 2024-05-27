ee9b907661706434c584c3dfcde7c949ab453556
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 10:02:49 +0900

    break文で終わる

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -4,11 +4,14 @@ public class Animation extends MyFrame
 	public void run()
 	{
 		int x =30;
-		while(x <= 170) 
+		while(true)
 		{
 			clear();
 			setColor(0,128,0);
 			fillRect(x,80,10,100);
+			if(x>200) {
+				break;
+			}
 			x+=20;
 			sleep(0.1);
 		}
