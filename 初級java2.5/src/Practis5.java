8fb43e9612cd490f2434ee90fd490f90db7a83d8
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 11:04:32 +0900

    練習問題5-3を追加

--- 初級java2.5/src/Practis5.java
+++ 初級java2.5/src/Practis5.java
@@ -3,14 +3,14 @@ public class Practis5 extends MyFrame {
 	public void run() 
 	{
 		int y =100,
-				x = 30;
-			while(y<=200)
+				x = 30,count = 0;
+		while(true) {
+			while(x<=170)
 			{
 				clear();
 				setColor(0,128,0);
 				fillRect(x,y,10,100);
-				y+=5;
-				x+=5;
+				x+=10;
 				sleep(0.1);
 			}
 			while(x>=30)
@@ -18,26 +18,16 @@ public class Practis5 extends MyFrame {
 				clear();
 				setColor(0,128,0);
 				fillRect(x,y,10,100);
-				x-=5;
+				x-=10;
 				sleep(0.1);
 			}
-			while(y>=100)
+			count +=1;
+			if(count == 3) 
 			{
-				clear();
-				setColor(0,128,0);
-				fillRect(x,y,10,100);
-				y-=5;
-				x+=5;
-				sleep(0.1);
+				break;
+			
 			}
-			while(x>=30)
-			{
-				clear();
-				setColor(0,128,0);
-				fillRect(x,y,10,100);
-				x-=5;
-				sleep(0.1);
-			}
+		}
 		
 	}
 
