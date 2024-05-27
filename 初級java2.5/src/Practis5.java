539c34797a4c451593f276a5800d5e2c4ff32cb4
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 11:41:28 +0900

    おまけ１

--- 初級java2.5/src/Practis5.java
+++ 初級java2.5/src/Practis5.java
@@ -3,7 +3,8 @@ public class Practis5 extends MyFrame {
 	public void run() 
 	{
 		int y =100,
-				x = 30,count = 0;
+				x = 30,count = 0,yp;
+		yp=y;
 		while(true) {
 			while(x<=170)
 			{
@@ -13,6 +14,15 @@ public class Practis5 extends MyFrame {
 				x+=10;
 				sleep(0.1);
 			}
+			while(y<=yp+100)
+			{
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				y+=10;
+				sleep(0.1);
+			}
+			yp=y;
 			while(x>=30)
 			{
 				clear();
@@ -21,6 +31,15 @@ public class Practis5 extends MyFrame {
 				x-=10;
 				sleep(0.1);
 			}
+			while(y<=yp+100)
+			{
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				y+=10;
+				sleep(0.1);
+			}
+			yp =y;
 			count +=1;
 			if(count == 3) 
 			{
