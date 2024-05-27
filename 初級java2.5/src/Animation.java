7a0a7f1d3d9aff1403b1af686195642793548664
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 10:19:17 +0900

    ５．１０までやった

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -3,13 +3,14 @@ public class Animation extends MyFrame
 {
 	public void run()
 	{
-		int y =100;
+		int y =100,
+				x = 30;
 		while(y<=200)
 		{
 			clear();
 			setColor(0,128,0);
-			fillRect(30,y,10,100);
-			y+=5;
+			fillRect(x,y,10,100);
+			x+=5;
 			sleep(0.1);
 		}
 	}
