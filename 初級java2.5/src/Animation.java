c30ff11a6dbc806aeffbf40cf096111a3d5a8cdb
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 10:20:29 +0900

    横に行ってから縦

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -5,12 +5,21 @@ public class Animation extends MyFrame
 	{
 		int y =100,
 				x = 30;
-		while(y<=200)
+		while(x<=170)
 		{
 			clear();
 			setColor(0,128,0);
 			fillRect(x,y,10,100);
 			x+=5;
+			sleep(0.1);
+		}
+		
+		while(y<=200)
+		{
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			y+=5;
 			sleep(0.1);
 		}
 	}
