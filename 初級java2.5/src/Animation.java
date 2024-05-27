204a81bae2e7adcb4cd0e46612e525d927e6139c
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-27 10:43:06 +0900

    右往左往する

--- 初級java2.5/src/Animation.java
+++ 初級java2.5/src/Animation.java
@@ -5,22 +5,24 @@ public class Animation extends MyFrame
 	{
 		int y =100,
 				x = 30;
-		while(x<=170)
-		{
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			x+=5;
-			sleep(0.1);
-		}
+		while(true) {
+			while(x<=170)
+			{
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x+=5;
+				sleep(0.1);
+			}
 		
-		while(y<=200)
-		{
-			clear();
-			setColor(0,128,0);
-			fillRect(x,y,10,100);
-			y+=5;
-			sleep(0.1);
+			while(x>=30)
+			{
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x-=5;
+				sleep(0.1);
+			}
 		}
 	}
 }
