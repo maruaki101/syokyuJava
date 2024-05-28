6e0b8c69073ca4269da7375ad90e7728a47cc9c8
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-05-28 12:06:01 +0900

    配列増やした

--- 初級java2.5/src/DisplayFaceObject.java
+++ 初級java2.5/src/DisplayFaceObject.java
@@ -3,17 +3,19 @@ public class DisplayFaceObject extends MyFrame
 {
 	public void run() 
 	{
-		Face[]faces = new Face[3];
+		Face[]faces = new Face[4];
 		 faces[0]= new Face(50,50,10,5);
 		
 		 faces[1]= new Face(200,100,-10,-5);
 		
 		 faces[2]= new Face(0,100,-10,-5);
+		 
+		 faces[3]= new Face(100,30,-10,-5);
 		
 		for(int i=1;i<30;i++) 
 		{
 			clear();
-			for(int j=0;j<3;j++) {
+			for(int j=0;j<faces.length;j++) {
 				faces[j].draw(this);
 				faces[j].move();
 			}
