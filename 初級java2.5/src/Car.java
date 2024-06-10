7f9b1478589c43b04f4bb557f1f4831d3d13250f
maruaki082 <mingwanmucun720@i.softbank.jp>
2024-06-10 09:49:39 +0900

    8-1追加

---
+++ 初級java2.5/src/Car.java
@@ -0,0 +1,24 @@
+
+public class Car 
+{
+	int x,y,vx,vy;
+	public Car(int x,int y, int vx,int vy) 
+	{
+		this.x=x;
+		this.y=y;
+		this.vx=vx;
+		this.vy=vy;
+	}
+	public void draw(MyFrame farame) 
+	{
+		farame.fillRect(x+20,y,40,20);
+		farame.fillRect(x,y+20,80,20);
+		farame.fillOval(x+10,y+40,20,20);
+		farame.fillOval(x+50,y+40,20,20);
+	}
+	public void move() 
+	{
+		x+=vx;
+		y+=vy;
+	}
+}
